9c0cf76b0fce4794173f906d92bc1d3a5a2515b2
Marvinftwzxc <marvincordova1738@gmail.com>
2023-06-08 19:41:49 +0800

    navigation
    pa check idolo matrix

--- index.php
+++ index.php
@@ -4,10 +4,85 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--AddDesign -->
+    <link rel="stylesheet" href="CSS/AddDesign.css">
+    <!--GoogleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200&family=Roboto+Condensed&family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <h1>Marvinxcz</h1>
+<nav class="navbar navbar-expand-lg bg-dark sticky-top " data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Jims Private Resort</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" target="_blank" href="View/domain/ContactUs.php">Contact Us</a>
+        </li>
+      </ul>
+   <!--For Register -->   
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link me-1" target="_blank" href="View/domain/register.php">Register</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link me-1" target="_blank" href="View/domain/login.php">Sign-in</a>
+    </li>
+  </ul>
+  <!--For Register --> 
+</div>q
+  </div>
+</nav>
+
+<location>
+<h1 class="text-center mt-5 font-family-roboto">Our Local Campus</h1>
+  <div class="Container justify-content-center d-flex mt-5">
+    <div class="image-container">
+      <div class="font-family-roboto text-center text-white transition-ImageLocation">
+    <h1 class="font-family-roboto mt-6">Nueva Ecija</h1>
+    </div>
+    <img class="Location-Image" src="View/Images/LocationImage.jpg" alt="location-image">
+  </div>
+  </div>
+</location>
+
+<picture>
+<div class="Container mt-5">
+<h1 class="text-center pt-3 font-family-roboto">Picture</h1>
+<div class="d-flex mt-5 justify-content-around">
+    <img class="custom-image" src="View/Images/picture-one.jpg" alt="Image 1">
+    <img class="custom-image" src="View/Images/picture-two.jpg" alt="Image 2">
+    <img class="custom-image" src="View/Images/picture-three.jpg" alt="Image 3">
+  </div>
+  </div>
+</picture>
+
+
+
+<!-- Optional JavaScript -->
+ <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
 
