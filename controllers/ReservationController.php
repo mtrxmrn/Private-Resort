51658752f02600d95cb993a10854be2cd1564e2e
mtrxmrn <100868511+mtrxmrn@users.noreply.github.com>
2023-06-20 02:40:15 +0800

    Reservation Email

--- controllers/ReservationController.php
+++ controllers/ReservationController.php
@@ -1,6 +1,13 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require_once 'PHPMailer/src/Exception.php';
+    require_once 'PHPMailer/src/PHPMailer.php';
+    require_once 'PHPMailer/src/SMTP.php';
     require_once 'Model/ModelToDatabase.php';
     class ReservationController extends ModelToDatabase{
+
         public function controller(){
 
             if(isset($_POST['submit'])){
@@ -26,15 +33,45 @@
                         $this->reserve($id, $fullname,$email,$phone,$date,$time,$numPersons,$notes);
                         $getDate = $this->getUserID($id);
                         $_SESSION['reservedate'] = $getDate['reservedate'];
+                        $this->emailuser($email,$fullname,$date);
                     }
                 }
-                
-                
-                
             }
-
             include 'View/ReservationView.php';
         }
+
+        public function emailuser($email, $fullname, $date){
+
+            $mail = new PHPMailer(true);
+
+        try {
+            // SMTP configuration for Gmail
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'mmatrixmariano1@gmail.com';
+            $mail->Password = 'ealinoqcgqyxiayj';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+
+            // Sender and recipient details
+            $mail->setFrom('mmatrixmariano1@gmail.com', "jim's Resort");
+            $mail->addAddress($email, $fullname);
+
+            // Email content
+            $mail->isHTML(true);
+            $mail->Subject = "Welcome to Jim's Resort";
+            $mail->Body = "Dear Mr/Mrs " . $_SESSION['lname']. '. We would like to inform you that your booking in: ' . $date . ' is on process. Please provide us supporting documents to officially verify your booking!. Thank you' ;
+
+            // Send the email
+            $mail->send();
+            return true; // Email sent successfully
+        } catch (Exception $e) {
+            return false; // Email could not be sent
+        }
+
+        }
+
     }
 
 ?>
