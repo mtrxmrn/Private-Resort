e806e608ef5e2892549041408130fe45a6894817
Marvinftwzxc <marvincordova1738@gmail.com>
2023-06-22 12:15:29 +0800

    updated (admin)

--- View/admin/AdminDeclined.php
+++ View/admin/AdminDeclined.php
@@ -1,40 +1,39 @@
-<div class="col-9">
-<div class="container justify-content d-flex ms-1 flex-column">
-    <table class="table table-striped">
-    <h2>Table Contents</h2>
+<div class="col-12 col-md-9">
+  <div class="container justify-content d-flex ms-1 flex-column">
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <h2>Table Contents</h2>
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>Email</th>
-                <th>Full Name</th>
-                <th>Reserve Date</th>
-                <th>Gcash Name</th>
-                <th>Gcash Number</th>
-                <th>Amount</th>
-                <th>Pending</th>
-                <th>Operation</th>
-                <!-- Add more table headers/columns as needed -->
-            </tr>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Full Name</th>
+            <th>Reserve Date</th>
+            <th>Gcash Name</th>
+            <th>Gcash Number</th>
+            <th>Amount</th>
+            <th>Pending</th>
+            <th>Operation</th>
+            <!-- Add more table headers/columns as needed -->
+          </tr>
         </thead>
         <tbody>
-            <?php foreach ($rows as $row) { ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['email']; ?></td>
-                    <td><?php echo $row['fullname']; ?></td>
-                    <td><?php echo $row['reservedate']; ?></td>
-                    <td><?php echo $row['gcashName']; ?></td>
-                    <td><?php echo $row['gcashNumber']; ?></td>
-                    <td><?php echo $row['amount']; ?></td>
-                    <td><?php echo "Declined"; ?></td>
-                    <td>
-
-
-                    </td>
-                    <!-- Add more table cells/columns as needed -->
-                </tr>
-            <?php } ?>
+          <?php foreach ($rows as $row) { ?>
+            <tr>
+              <td><?php echo $row['id']; ?></td>
+              <td><?php echo $row['email']; ?></td>
+              <td><?php echo $row['fullname']; ?></td>
+              <td><?php echo $row['reservedate']; ?></td>
+              <td><?php echo $row['gcashName']; ?></td>
+              <td><?php echo $row['gcashNumber']; ?></td>
+              <td><?php echo $row['amount']; ?></td>
+              <td><?php echo "Declined"; ?></td>
+              <td></td>
+              <!-- Add more table cells/columns as needed -->
+            </tr>
+          <?php } ?>
         </tbody>
-    </table>
-</div>
+      </table>
+    </div>
+  </div>
 </div>
--- View/admin/AdminPending.php
+++ View/admin/AdminPending.php
@@ -1,45 +1,43 @@
- <!-- Table Contents -->
- <div class="col-9">
-      <div class="container justify-content d-flex ms-1 flex-column">
-        <table class="table table-striped">
-          <h2 class="">Table Contents</h2>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Email</th>
-              <th>Full Name</th>
-              <th>Reserve Date</th>
-              <th>Gcash Name</th>
-              <th>Gcash Number</th>
-              <th>Amount</th>
-              <th>Pending</th>
-              <th>Operation</th>
-                <!-- Add more table headers/columns as needed -->
-            </tr>
+<div class="col-12 col-md-9">
+  <div class="container justify-content d-flex ms-1 flex-column">
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <h2 class="">Table Contents</h2>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Full Name</th>
+            <th>Reserve Date</th>
+            <th>Gcash Name</th>
+            <th>Gcash Number</th>
+            <th>Amount</th>
+            <th>Pending</th>
+            <th>Operation</th>
+          </tr>
         </thead>
         <tbody>
-            <?php foreach ($rows as $row) { ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['email']; ?></td>
-                    <td><?php echo $row['fullname']; ?></td>
-                    <td><?php echo $row['reservedate']; ?></td>
-                    <td><?php echo $row['gcashName']; ?></td>
-                    <td><?php echo $row['gcashNumber']; ?></td>
-                    <td><?php echo $row['amount']; ?></td>
-                    <td><?php echo "Pending" ?></td>
-                    <td>
-                    <form action="Admin.php?page=pending" method="POST">
-                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                        <button type="submit" name="confirm" class="btn btn-success">CONFIRM</button>
-                        <button type="submit" name="decline" class="btn btn-danger">DECLINE</button>
-                    </form>
-
-                    </td>
-                    <!-- Add more table cells/columns as needed -->
-                </tr>
-            <?php } ?>
+          <?php foreach ($rows as $row) { ?>
+            <tr>
+              <td><?php echo $row['id']; ?></td>
+              <td><?php echo $row['email']; ?></td>
+              <td><?php echo $row['fullname']; ?></td>
+              <td><?php echo $row['reservedate']; ?></td>
+              <td><?php echo $row['gcashName']; ?></td>
+              <td><?php echo $row['gcashNumber']; ?></td>
+              <td><?php echo $row['amount']; ?></td>
+              <td><?php echo "Pending" ?></td>
+              <td>
+                <form action="Admin.php?page=pending" method="POST">
+                  <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                  <button type="submit" name="confirm" class="btn btn-success">CONFIRM</button>
+                  <button type="submit" name="decline" class="btn btn-danger">DECLINE</button>
+                </form>
+              </td>
+            </tr>
+          <?php } ?>
         </tbody>
-    </table>
+      </table>
+    </div>
+  </div>
 </div>
- </div>
